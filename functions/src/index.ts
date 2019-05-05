947815f3206ceca51d360e096b8f00df9dee951e
지준우 <junwoo@finda.co.kr>
2019-05-05 16:28:49 +0900

    add getShortUrl function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,16 @@
 import * as functions from 'firebase-functions';
+import { Response } from 'express';
+import { TypedRequest } from './main/model/Types';
+import { GetShortUrlRequest } from './generate/model/ShortUrlRequest';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+export const getShortUrl = functions.https.onRequest((request: TypedRequest<GetShortUrlRequest>, response: Response) => {
+    const { url, postfix } = request.body;
+    response.send(`Hello World! url: ${url} postfix: ${postfix} good!!!`);
+});
