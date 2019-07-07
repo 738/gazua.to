d96828d6724c16e37d0325cd82dc129736aa5476
Jon Jee <jydrogen@gmail.com>
2019-07-08 00:12:45 +0900

    #2 postfix 없는 경우 처리

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin';
 import { Response } from 'express';
 import { TypedRequest } from './main/model/Types';
 import { GetShortUrlRequest } from './generate/model/ShortUrlRequest';
-// import { getRandomPostfix } from './util/GeneralUtil';
+import { getRandomPostfix } from './util/GeneralUtil';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -39,6 +39,28 @@ export const getShortUrl = functions.https.onRequest(async (request: TypedReques
                 url,
             });
         }
+    } else {
+        let short_url: string = getRandomPostfix(6);
+        while (1) {
+            const snapshot = await db.collection('gazua-to').get();
+            let isTaken = false;
+            snapshot.forEach(doc => {
+                if (doc.id === short_url) {
+                    isTaken = true;
+                }
+            });
+            if (isTaken) {
+                console.log('isTaken = true!!!');
+                short_url = getRandomPostfix(6);
+                continue;
+            } else break;
+        }
+        let docRef = db.collection('gazua-to').doc(short_url);
+        // let setShortUrlResult: FirebaseFirestore.WriteResult = 
+        await docRef.set({
+            short_url,
+            url,
+        });
     }
 
     // let docRef = db.collection('gazua-to').doc(short_url);
